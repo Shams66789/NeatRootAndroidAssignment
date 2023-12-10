9b28c53fd1d09b86e9e235525a173e519538fd17
Shams66789 <rajansari66789@gmail.com>
2023-12-10 16:10:31 +0530

    UI_2 2nd page done

--- UI_2/app/src/main/java/com/example/ui_2/MainActivity.kt
+++ UI_2/app/src/main/java/com/example/ui_2/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.ui_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView5)
+
+        textView.setOnClickListener() {
+            intent = Intent(this,Signin::class.java)
+            startActivity(intent)
+        }
+
     }
 }
