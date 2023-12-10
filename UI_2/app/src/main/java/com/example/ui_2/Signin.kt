ccbf2fcdec7d5203da78027a44779884b3c66911
Shams66789 <rajansari66789@gmail.com>
2023-12-10 17:15:15 +0530

    UI_2 3rd page done

--- UI_2/app/src/main/java/com/example/ui_2/Signin.kt
+++ UI_2/app/src/main/java/com/example/ui_2/Signin.kt
@@ -1,11 +1,20 @@
 package com.example.ui_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Signin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+        val textView = findViewById<Button>(R.id.button)
+
+        textView.setOnClickListener() {
+            intent = Intent(this,Register::class.java)
+            startActivity(intent)
+        }
     }
 }
